c77a9e4c655ea4732950a8fb7c33680350857b34
Mike Pall <mike>
2012-11-14 21:54:27 +0100

    Don't clear frame for out-of-memory error.

--- src/lj_err.c
+++ src/lj_err.c
@@ -485,7 +485,6 @@ LJ_NOINLINE void lj_err_mem(lua_State *L)
 {
   if (L->status == LUA_ERRERR+1)  /* Don't touch the stack during lua_open. */
     lj_vm_unwind_c(L->cframe, LUA_ERRMEM);
-  L->top = L->base;
   setstrV(L, L->top++, lj_err_str(L, LJ_ERR_ERRMEM));
   lj_err_throw(L, LUA_ERRMEM);
 }
