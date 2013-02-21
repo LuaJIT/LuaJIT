61fb587d2c1646cae4c90990b9c4c1f1bff09e5b
Mike Pall <mike>
2013-02-21 16:49:04 +0100

    BRANCH LuaJIT-2.1

--- src/luajit.h
+++ src/luajit.h
@@ -30,9 +30,9 @@
 
 #include "lua.h"
 
-#define LUAJIT_VERSION		"LuaJIT 2.0.1"
-#define LUAJIT_VERSION_NUM	20001  /* Version 2.0.1 = 02.00.01. */
-#define LUAJIT_VERSION_SYM	luaJIT_version_2_0_1
+#define LUAJIT_VERSION		"LuaJIT 2.1.0-alpha"
+#define LUAJIT_VERSION_NUM	20100  /* Version 2.1.0 = 02.01.00. */
+#define LUAJIT_VERSION_SYM	luaJIT_version_2_1_0_alpha
 #define LUAJIT_COPYRIGHT	"Copyright (C) 2005-2013 Mike Pall"
 #define LUAJIT_URL		"http://luajit.org/"
 
