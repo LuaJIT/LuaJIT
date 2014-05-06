29172880192471b12e699718a906ff1b9a270fe4
Mike Pall <mike>
2014-05-06 14:47:37 +0200

    x64: Workaround for MSVC build issue.

--- src/lj_err.c
+++ src/lj_err.c
@@ -378,7 +378,7 @@ typedef struct UndocumentedDispatcherContext {
   ULONG64 EstablisherFrame;
   ULONG64 TargetIp;
   PCONTEXT ContextRecord;
-  PEXCEPTION_ROUTINE LanguageHandler;
+  void (*LanguageHandler)(void);
   PVOID HandlerData;
   PUNWIND_HISTORY_TABLE HistoryTable;
   ULONG ScopeIndex;
