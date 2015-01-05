86913b9bbf75c78a5dcfa2bf6071db8db7df7c51
Mike Pall <mike>
2015-01-05 23:59:31 +0100

    Bump copyright date to 2015.

--- src/luaconf.h
+++ src/luaconf.h
@@ -1,6 +1,6 @@
 /*
 ** Configuration header.
-** Copyright (C) 2005-2014 Mike Pall. See Copyright Notice in luajit.h
+** Copyright (C) 2005-2015 Mike Pall. See Copyright Notice in luajit.h
 */
 
 #ifndef luaconf_h
