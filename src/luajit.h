fb77f7dee7fff698736b48392a6057c60d0d7c2b
Mike Pall <mike>
2015-08-25 23:32:36 +0200

    RELEASE LuaJIT-2.1.0-beta1

--- src/luajit.h
+++ src/luajit.h
@@ -30,9 +30,9 @@
 
 #include "lua.h"
 
-#define LUAJIT_VERSION		"LuaJIT 2.1.0-alpha"
+#define LUAJIT_VERSION		"LuaJIT 2.1.0-beta1"
 #define LUAJIT_VERSION_NUM	20100  /* Version 2.1.0 = 02.01.00. */
-#define LUAJIT_VERSION_SYM	luaJIT_version_2_1_0_alpha
+#define LUAJIT_VERSION_SYM	luaJIT_version_2_1_0_beta1
 #define LUAJIT_COPYRIGHT	"Copyright (C) 2005-2015 Mike Pall"
 #define LUAJIT_URL		"http://luajit.org/"
 
