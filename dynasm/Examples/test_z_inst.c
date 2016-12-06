7cc069ef367169c3af0d1dde54c7e89eeefe0733
Michael Munday <munday@ca.ibm.com>
2016-12-06 13:15:29 -0500

    Support floating point register arguments.
    It would be nice to verify that floating-point/general-purpose
    registers are indeed expected by the instruction, but for now treat
    them both the same so we can use floating-point instructions.

--- dynasm/Examples/test_z_inst.c
+++ dynasm/Examples/test_z_inst.c
@@ -101,12 +101,72 @@ static void add_imm16(dasm_State *state)
   | br r14
 }
 
-
 static void add_imm32(dasm_State *state)
 {
   dasm_State **Dst = &state;
   
   | afi r2 , 0xe
+  | br r14
+}
+
+static void save(dasm_State *state)
+{
+  dasm_State **Dst = &state;
+
+  |.define CFRAME_SPACE,	224	// Delta for sp, 8 byte aligned.
+  |
+  |// Register save area.
+  |.define SAVE_GPRS,	264(sp)	// Save area for r6-r15 (10*8 bytes).
+  |
+  |// Argument save area, each slot is 8-bytes (32-bit types are sign/zero extended).
+  |.define RESERVED,	232(sp)	// Reserved for compiler use.
+  |.define BACKCHAIN,	224(sp)
+  |
+  |// Current stack frame.
+  |.define SAVE_FPR15,	216(sp)
+  |.define SAVE_FPR14,	208(sp)
+  |.define SAVE_FPR13,	200(sp)
+  |.define SAVE_FPR12,	192(sp)
+  |.define SAVE_FPR11,	184(sp)
+  |.define SAVE_FPR10,	176(sp)
+  |.define SAVE_FPR9,	168(sp)
+  |.define SAVE_FPR8,	160(sp)
+  |
+  |// Callee save area.
+  |.define CALLEESAVE,	000(sp)
+  |
+  |.macro saveregs
+  |  lay sp, -CFRAME_SPACE(sp)	// Allocate stack frame.
+  |  stmg r6, r15, SAVE_GPRS	// Technically we restore r15 regardless.
+  |  std f8, SAVE_FPR8		// f8-f15 are callee-saved.
+  |  std f9, SAVE_FPR9
+  |  std f10, SAVE_FPR10
+  |  std f11, SAVE_FPR11
+  |  std f12, SAVE_FPR12
+  |  std f13, SAVE_FPR13
+  |  std f14, SAVE_FPR14
+  |  std f15, SAVE_FPR15
+  |.endmacro
+  |
+  |.macro restoreregs
+  |  ld f8, SAVE_FPR8		// f8-f15 are callee-saved.
+  |  ld f9, SAVE_FPR9
+  |  ld f10, SAVE_FPR10
+  |  ld f11, SAVE_FPR11
+  |  ld f12, SAVE_FPR12
+  |  ld f13, SAVE_FPR13
+  |  ld f14, SAVE_FPR14
+  |  ld f15, SAVE_FPR15
+  |  lmg r6, r15, SAVE_GPRS	// Restores the stack pointer.
+  |.endmacro
+  |
+  | saveregs
+  | lgfi r7, 10 // 16
+  | lgfi r8, 20 // 32
+  | agr r2, r3
+  | agr r7, r8
+  | msgr r2, r7
+  | restoreregs
   | br r14
 }
 
@@ -127,7 +187,8 @@ test_table test[] = {
   { 2, 4,       lab,     32,   "lab"},
   { 2, 4,      labg,     32,  "labg"},
   { 2, 0, add_imm16,     17, "imm16"},
-  { 2, 0, add_imm32,     16, "imm32"}
+  { 2, 0, add_imm32,     16, "imm32"},
+  { 7, 3,      save,    480,  "save"}
 };
 
 static void *jitcode(dasm_State **state, size_t *size)
