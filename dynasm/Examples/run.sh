111b62229a287f255c79ad25a8de97bafd51ce33
Michael Munday <munday@ca.ibm.com>
2016-12-09 14:38:37 -0500

    Define DASM_CHECKS when running tests.

--- dynasm/Examples/run.sh
+++ dynasm/Examples/run.sh
@@ -2,7 +2,7 @@
 # set -x
 
 # run test
-lua ../dynasm.lua test_z_inst.c | gcc -std=gnu99 -Wall -Werror -g -x c -o test_z_inst -
+lua ../dynasm.lua test_z_inst.c | gcc -DDASM_CHECKS -std=gnu99 -Wall -Werror -g -x c -o test_z_inst -
 ./test_z_inst
 ec=$?
 
