79d31b755741cdd1ba742a51441923b6d14e9562
Francois Perrad <francois.perrad@gadz.org>
2017-03-31 15:12:30 +0200

    lexico

--- src/lj_lex.h
+++ src/lj_lex.h
@@ -16,7 +16,8 @@
   _(and) _(break) _(do) _(else) _(elseif) _(end) _(false) \
   _(for) _(function) _(goto) _(if) _(in) _(local) _(nil) _(not) _(or) \
   _(repeat) _(return) _(then) _(true) _(until) _(while) \
-  __(concat, ..) __(dots, ...) __(eq, ==) __(ge, >=) __(le, <=) __(ne, ~=) \
+  __(idiv, idiv) __(concat, ..) __(dots, ...) __(eq, ==) __(ge, >=) __(le, <=) __(ne, ~=) \
+  __(shl, <<) __(shr, >>) \
   __(label, ::) __(number, <number>) __(name, <name>) __(string, <string>) \
   __(eof, <eof>)
 
