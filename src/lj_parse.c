04dc66a794d196bad63c3e91db67250acb677458
Francois Perrad <francois.perrad@gadz.org>
2017-03-31 16:20:25 +0200

    define OPR

--- src/lj_parse.c
+++ src/lj_parse.c
@@ -149,6 +149,9 @@ typedef struct FuncState {
 typedef enum BinOpr {
   OPR_ADD, OPR_SUB, OPR_MUL, OPR_DIV, OPR_MOD, OPR_POW,  /* ORDER ARITH */
   OPR_CONCAT,
+  OPR_IDIV,
+  OPR_BAND, OPR_BOR, OPR_BXOR,
+  OPR_SHL, OPR_SHR,
   OPR_NE, OPR_EQ,
   OPR_LT, OPR_GE, OPR_LE, OPR_GT,
   OPR_AND, OPR_OR,
