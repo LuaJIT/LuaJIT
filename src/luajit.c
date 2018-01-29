03cd5aa749c1bc3bb4b7d4289236b6096cb3dc85
Mike Pall <mike>
2018-01-29 13:25:51 +0100

    Clear stack after print_jit_status() in CLI.
    Suggested by Hydroque.

--- src/luajit.c
+++ src/luajit.c
@@ -151,6 +151,7 @@ static void print_jit_status(lua_State *L)
     fputs(s, stdout);
   }
   putc('\n', stdout);
+  lua_settop(L, 0);  /* clear stack */
 }
 
 static int getargs(lua_State *L, char **argv, int n)
