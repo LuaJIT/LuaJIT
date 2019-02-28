a61c93d0784c532db4ec0797475a0e0ad93dda4c
Yichun Zhang (agentzh) <yichun@openresty.com>
2019-02-27 17:20:19 -0800

    bugfix: ffi.C.FUNC(): it lacked a write barrier which might lead to use-after-free issues and memory corruptions.
    Fix #42.

--- src/lj_clib.c
+++ src/lj_clib.c
@@ -384,6 +384,7 @@ TValue *lj_clib_index(lua_State *L, CLibrary *cl, GCstr *name)
       cd = lj_cdata_new(cts, id, CTSIZE_PTR);
       *(void **)cdataptr(cd) = p;
       setcdataV(L, tv, cd);
+      lj_gc_anybarriert(L, cl->cache);
     }
   }
   return tv;
