0f898472a2d545fa720d49abf4440134e5bdfc03
s.ostanevich <s.ostanevich@sostanevich.local>
2019-09-05 17:05:34 +0300

    follow-up for the LUAJit folder problem
    The original patch did not provide correct ending of patch from
    string:find() call. Sent to review to @mraleph as part of
    LUAJit gh-505
    
    fixes: #4476

--- src/lj_ffrecord.c
+++ src/lj_ffrecord.c
@@ -953,7 +953,7 @@ static void LJ_FASTCALL recff_string_find(jit_State *J, RecordFFData *rd)
       /* Caveat: can't use STRREF trstr 0 here because that might be pointing into a wrong string due to folding. */
       pos = emitir(IRTI(IR_SUB), tr, trsptr);
       J->base[0] = emitir(IRTI(IR_ADD), pos, emitir(IRTI(IR_ADD), trstart, lj_ir_kint(J, 1)));
-      J->base[1] = emitir(IRTI(IR_ADD), pos, trplen);
+      J->base[1] = emitir(IRTI(IR_ADD), pos, emitir(IRTI(IR_ADD), trplen, trstart));
       rd->nres = 2;
     } else {
       emitir(IRTG(IR_EQ, IRT_PGC), tr, trp0);
