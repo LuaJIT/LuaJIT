6a91b0dd5d136f7f61d172e76881f5e47cd55e27
Siddhesh Poyarekar <siddhesh@gotplt.org>
2019-09-24 04:52:29 -0700

    Add test for string.find
    This test is for LuaJIT/LuaJIT#505
    
    Signed-off-by: s.ostanevich <s.ostanevich@sostanevich.local>

---
+++ test/lib/string/find.lua
@@ -0,0 +1,10 @@
+do --- find relative
+  jit.opt.start("hotloop=1")
+  for _ = 1, 20 do
+    local value = "abc"
+    local pos_c = string.find(value, "c", 1, true)
+    local value2 = string.sub(value, 1, pos_c - 1)
+    local pos_b = string.find(value2, "b", 2, true)
+    assert(pos_b == 2, "FAIL: position of 'b' is " .. pos_b)
+  end
+end
