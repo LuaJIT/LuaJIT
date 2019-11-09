b600811231bac7e267dbac878b9690559b6f74e8
Francois Perrad <francois.perrad@gadz.org>
2019-11-09 14:02:47 +0100

    backport lua_pushglobaltable from 5.2

--- src/lua.h
+++ src/lua.h
@@ -280,6 +280,8 @@ LUA_API void lua_setallocf (lua_State *L, lua_Alloc f, void *ud);
 
 #define lua_tostring(L,i)	lua_tolstring(L, (i), NULL)
 
+/* From Lua 5.2. */
+#define lua_pushglobaltable(L)	lua_pushvalue(L, LUA_GLOBALSINDEX)
 
 
 /*
