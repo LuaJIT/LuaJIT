18f13e2b1f2aadd35dd87ed9be4b4f92e2243af5
Girish Joshi <girish946@gmail.com>
2020-03-30 16:41:08 +0530

    enabled tests under test/misc. enabled hook_line.lua and hook_record.lua
    Signed-off-by: Girish Joshi <girish946@gmail.com>

--- test/misc/hook_line.lua
+++ test/misc/hook_line.lua
@@ -1,41 +1,44 @@
-local lines = {}
-local function hook()
-  lines[#lines+1] = debug.getinfo(2).currentline
+do --- hook-line
+  local lines = {}
+  local function hook()
+    lines[#lines+1] = debug.getinfo(2).currentline
+  end
+
+  local function dummy()
+  end -- <-- line 7
+
+  debug.sethook(hook, "l", 0)
+  -- <-- line 10
+  local x
+  dummy()
+  local y = 1
+  dummy() dummy()
+  local z = 2; local r = true
+  while y < 4 do y = y + 1 end
+  while z < 4 do
+    z = z + 1
+  end
+  -- <-- line 20
+  local v
+  debug.sethook(nil, "", 0)
+
+  assert(#lines > 0)
+  while lines[1] < 10 do table.remove(lines, 1) end
+  while lines[#lines] > 20 do table.remove(lines) end
+
+  local s = table.concat(lines, " ")
+  assert(s == "11 12 7 13 14 7 7 15 16 16 16 16 17 18 17 18 17" or
+         s == "11 12 7 13 14 7 14 7 15 16 16 16 16 17 18 17 18 17" or
+         s == "12 13 8 14 15 8 15 8 16 17 17 17 17 18 19 18 19 18"
+         )
+
+  lines = {}
+  local function f()
+    if true then return end
+    local function x() end
+  end -- <-- line 36
+  debug.sethook(hook, "l", 0)
+  f()
+  debug.sethook(nil, "", 0)
+  for i=1,#lines do assert(lines[i] ~= 36) end
 end
-
-local function dummy()
-end -- <-- line 7
-
-debug.sethook(hook, "l", 0)
--- <-- line 10
-local x
-dummy()
-local y = 1
-dummy() dummy()
-local z = 2; local r = true
-while y < 4 do y = y + 1 end
-while z < 4 do
-  z = z + 1
-end
--- <-- line 20
-local v
-debug.sethook(nil, "", 0)
-
-assert(#lines > 0)
-while lines[1] < 10 do table.remove(lines, 1) end
-while lines[#lines] > 20 do table.remove(lines) end
-
-local s = table.concat(lines, " ")
-assert(s == "11 12 7 13 14 7 7 15 16 16 16 16 17 18 17 18 17" or
-       s == "11 12 7 13 14 7 14 7 15 16 16 16 16 17 18 17 18 17")
-
-lines = {}
-local function f()
-  if true then return end
-  local function x() end
-end -- <-- line 36
-debug.sethook(hook, "l", 0)
-f()
-debug.sethook(nil, "", 0)
-for i=1,#lines do assert(lines[i] ~= 36) end
-
--- test/misc/hook_record.lua
+++ test/misc/hook_record.lua
@@ -1,8 +1,8 @@
+do --- hook-record
+  if not jit or not jit.status or not jit.status() then return end
 
-if not jit or not jit.status or not jit.status() then return end
-
-debug.sethook(function() for i=1,100 do end end, "", 10)
-for i=1,10 do end
-debug.sethook()
-assert((require("jit.util").traceinfo(1)))
-
+  debug.sethook(function() for i=1,100 do end end, "", 10)
+  for i=1,10 do end
+  debug.sethook()
+  assert((require("jit.util").traceinfo(1)))
+end
