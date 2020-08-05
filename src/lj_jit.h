2211f6f960b65d200a3142798cf86576405c24cb
Mike Pall <mike>
2020-08-05 15:21:00 +0200

    ARM: Ensure relative GG_State element alignment differently.
    Thanks to jojo59516 and dwing4g.

--- src/lj_jit.h
+++ src/lj_jit.h
@@ -406,11 +406,7 @@ typedef struct jit_State {
   size_t szallmcarea;	/* Total size of all allocated mcode areas. */
 
   TValue errinfo;	/* Additional info element for trace errors. */
-}
-#if LJ_TARGET_ARM
-LJ_ALIGN(16)		/* For DISPATCH-relative addresses in assembler part. */
-#endif
-jit_State;
+} jit_State;
 
 /* Trivial PRNG e.g. used for penalty randomization. */
 static LJ_AINLINE uint32_t LJ_PRNG_BITS(jit_State *J, int bits)
