050466552bf7eb8b7944aa52c93b45326ae14d7a
Mike Pall <mike>
2020-10-12 16:11:32 +0200

    Minor changes and https-ify links.

--- dynasm/dynasm.lua
+++ dynasm/dynasm.lua
@@ -14,7 +14,7 @@ local _info = {
   vernum =	 10300,
   release =	"2011-05-05",
   author =	"Mike Pall",
-  url =		"http://luajit.org/dynasm.html",
+  url =		"https://luajit.org/dynasm.html",
   license =	"MIT",
   copyright =	[[
 Copyright (C) 2005-2020 Mike Pall. All rights reserved.
@@ -38,7 +38,7 @@ CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-[ MIT license: http://www.opensource.org/licenses/mit-license.php ]
+[ MIT license: https://www.opensource.org/licenses/mit-license.php ]
 ]],
 }
 
