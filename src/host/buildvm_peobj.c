8023980b9298ee7fc76ac96a08df53979892c361
Stephen Just <stephenjust@gmail.com>
2023-02-24 19:36:55 -0800

    Apply patch from `vanc`
    I'm not sure about the relocation offset, I've tried a couple of values, no difference.

--- src/host/buildvm_peobj.c
+++ src/host/buildvm_peobj.c
@@ -9,7 +9,7 @@
 #include "buildvm.h"
 #include "lj_bc.h"
 
-#if LJ_TARGET_X86ORX64 || LJ_TARGET_ARM64
+#if LJ_TARGET_WINDOWS
 
 /* Context for PE object emitter. */
 static char *strtab;
@@ -76,6 +76,19 @@ typedef struct PEsymaux {
   uint8_t unused[3];
 } PEsymaux;
 
+#if LJ_TARGET_ARM64
+#pragma pack(push, 1)
+typedef struct UnwindInfo {
+  uint32_t FunctionLength : 18;
+  uint32_t Version : 2;
+  uint32_t X : 1;
+  uint32_t E : 1;
+  uint32_t EpilogCount : 5;
+  uint32_t CodeWords : 5;
+} UnwindInfo;
+#pragma pack(pop)
+#endif
+
 /* Cannot use sizeof, because it pads up to the max. alignment. */
 #define PEOBJ_SYM_SIZE	(8+4+2+2+1+1)
 
@@ -95,7 +108,7 @@ typedef struct PEsymaux {
 #define PEOBJ_TEXT_FLAGS	0x60500020  /* 60=r+x, 50=align16, 20=code. */
 #elif LJ_TARGET_ARM64
 #define PEOBJ_ARCH_TARGET	0xaa64
-#define PEOBJ_RELOC_REL32	0x11  /* MS: REL32, GNU: DISP32. */
+#define PEOBJ_RELOC_REL32	0x03  /* MS: BRANCH26 */
 #define PEOBJ_RELOC_DIR32	0x01
 #define PEOBJ_RELOC_ADDR32NB	0x02
 #define PEOBJ_RELOC_OFS		0
@@ -107,7 +120,7 @@ enum {
   PEOBJ_SECT_ABS = -2,
   PEOBJ_SECT_UNDEF = -1,
   PEOBJ_SECT_TEXT,
-#if LJ_TARGET_X64
+#if LJ_TARGET_X64 || LJ_TARGET_ARM64
   PEOBJ_SECT_PDATA,
   PEOBJ_SECT_XDATA,
 #elif LJ_TARGET_X86
@@ -195,18 +208,28 @@ void emit_peobj(BuildCtx *ctx)
   /* Flags: 60 = read+execute, 50 = align16, 20 = code. */
   pesect[PEOBJ_SECT_TEXT].flags = PEOBJ_TEXT_FLAGS;
 
-#if LJ_TARGET_X64
+#if LJ_TARGET_X64 || LJ_TARGET_ARM64
   memcpy(pesect[PEOBJ_SECT_PDATA].name, ".pdata", sizeof(".pdata")-1);
   pesect[PEOBJ_SECT_PDATA].ofs = sofs;
+  #if LJ_TARGET_X64
   sofs += (pesect[PEOBJ_SECT_PDATA].size = 6*4);
   pesect[PEOBJ_SECT_PDATA].relocofs = sofs;
   sofs += (pesect[PEOBJ_SECT_PDATA].nreloc = 6) * PEOBJ_RELOC_SIZE;
+  #else
+  sofs += (pesect[PEOBJ_SECT_PDATA].size = 16);
+  pesect[PEOBJ_SECT_PDATA].relocofs = sofs;
+  sofs += (pesect[PEOBJ_SECT_PDATA].nreloc = 4) * PEOBJ_RELOC_SIZE;
+  #endif
   /* Flags: 40 = read, 30 = align4, 40 = initialized data. */
   pesect[PEOBJ_SECT_PDATA].flags = 0x40300040;
 
   memcpy(pesect[PEOBJ_SECT_XDATA].name, ".xdata", sizeof(".xdata")-1);
   pesect[PEOBJ_SECT_XDATA].ofs = sofs;
+#if LJ_TARGET_X64
   sofs += (pesect[PEOBJ_SECT_XDATA].size = 8*2+4+6*2);  /* See below. */
+#else
+  sofs += (pesect[PEOBJ_SECT_XDATA].size = sizeof(UnwindInfo)+44+4+4+/**/+sizeof(UnwindInfo)+8);  /* See below. */
+#endif
   pesect[PEOBJ_SECT_XDATA].relocofs = sofs;
   sofs += (pesect[PEOBJ_SECT_XDATA].nreloc = 1) * PEOBJ_RELOC_SIZE;
   /* Flags: 40 = read, 30 = align4, 40 = initialized data. */
@@ -241,7 +264,7 @@ void emit_peobj(BuildCtx *ctx)
   */
   nrsym = ctx->nrelocsym;
   pehdr.nsyms = 1+PEOBJ_NSECTIONS*2 + 1+ctx->nsym + nrsym;
-#if LJ_TARGET_X64
+#if LJ_TARGET_X64 || LJ_TARGET_ARM64
   pehdr.nsyms += 1;  /* Symbol for lj_err_unwind_win. */
 #endif
 
@@ -264,9 +287,11 @@ void emit_peobj(BuildCtx *ctx)
     owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
   }
 
-#if LJ_TARGET_X64
+#if LJ_TARGET_X64 || LJ_TARGET_ARM64
   { /* Write .pdata section. */
+    PEreloc reloc;
     uint32_t fcofs = (uint32_t)ctx->sym[ctx->nsym-1].ofs;
+    #if LJ_TARGET_X64
     uint32_t pdata[3];  /* Start of .text, end of .text and .xdata. */
     PEreloc reloc;
     pdata[0] = 0; pdata[1] = fcofs; pdata[2] = 0;
@@ -291,7 +316,34 @@ void emit_peobj(BuildCtx *ctx)
     reloc.vaddr = 20; reloc.symidx = 1+2+nrsym+2;
     reloc.type = PEOBJ_RELOC_ADDR32NB;
     owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
+    #else
+    uint32_t pdata[4];
+    pdata[0] = 0;
+    pdata[1] = 0;
+    pdata[2] = fcofs;
+    pdata[3] = sizeof(UnwindInfo)+44+4+4;
+    owrite(ctx, &pdata, sizeof(pdata));
+
+    /* Mark the .text */
+    reloc.vaddr = 0; reloc.symidx = 1+2+nrsym+2+2+1;
+    reloc.type = PEOBJ_RELOC_ADDR32NB;
+    owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
+    /* Mark the exception VA address as in .xdata section */
+    reloc.vaddr = 4; reloc.symidx = 1+2+nrsym+2;
+    reloc.type = PEOBJ_RELOC_ADDR32NB;
+    owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
+
+    /* Mark the vm_ffi_call function which requires special unwinding */
+    reloc.vaddr = 8; reloc.symidx = 1+2+nrsym+2+2+1;
+    reloc.type = PEOBJ_RELOC_ADDR32NB;
+    owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
+    /* Mark the exception VA address as in .xdata section */
+    reloc.vaddr = 12; reloc.symidx = 1+2+nrsym+2;
+    reloc.type = PEOBJ_RELOC_ADDR32NB;
+    owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
+    #endif
   }
+#if LJ_TARGET_X64
   { /* Write .xdata section. */
     uint16_t xdata[8+2+6];
     PEreloc reloc;
@@ -315,6 +367,110 @@ void emit_peobj(BuildCtx *ctx)
     reloc.type = PEOBJ_RELOC_ADDR32NB;
     owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
   }
+#else
+  { /* Write .xdata section. */
+    UnwindInfo ui = {0};
+    uint32_t handler[2];
+    uint8_t codes[64];
+    uint8_t *p;
+    PEreloc reloc;
+    uint32_t fcofs = (uint32_t)ctx->sym[ctx->nsym-1].ofs;
+
+    /* https://learn.microsoft.com/en-us/cpp/build/arm64-exception-handling?view=msvc-170#unwind-codes */
+
+    /* 000xxxxx: allocate small stack with size < 512 (2^5 * 16). */
+    #define ALLOC_S(size) do { \
+      *p++ = ((size) / 16) & 0x1f; \
+    } while(0)
+    /* 01zzzzzz: save <x29,lr> pair at [sp+#Z*8], offset <= 504. */
+    #define SAVE_FPLR(offs) do { \
+      *p++ = 0x40 | (((offs) / 8) & 0x1f); \
+    } while(0)
+    /* 110100xx'xxzzzzzz: save reg x(19+#X) at [sp+#Z*8], offset <= 504 */
+    #define SAVE_REG(reg, offs) do { \
+      uint16_t code = 0xd000 | (((reg) - 19) << 6) | ((offs) / 8); \
+      WRITE_CODE16(code); \
+    } while (0)
+    /* 1101110x'xxzzzzzz: save reg d(8+#X) at [sp+#Z*8], offset <= 504 */
+    #define SAVE_FREG(reg, offs) do { \
+      uint16_t code = 0xdc00 | (((reg) - 8) << 6) | ((offs) / 8); \
+      WRITE_CODE16(code); \
+    } while (0)
+    /* 1101010x'xxxzzzzz: save reg x(19+#X) at [sp-(#Z+1)*8]!, pre-indexed offset >= -256 */
+    #define SAVE_REG_X(reg, offs) do { \
+      uint16_t code = 0xd400 | (((reg) - 19) << 5) | (((offs * -1) / 8) - 1); \
+      WRITE_CODE16(code); \
+    } while (0)
+    /* 11100010'xxxxxxxx: set up x29 with add x29,sp,#x*8 */
+    #define ADD_FP(size) do { \
+      uint16_t code = 0xe200 | (((size) / 8) & 0xff); \
+      WRITE_CODE16(code); \
+    } while(0)
+    #define WRITE_CODE16(code) do { \
+      /* the 2-byte code is saved in big endian */ \
+      *p++ = ((code) & 0xff00) >> 8; \
+      *p++ = ((code) & 0xff); \
+    } while (0)
+
+    /* fill the codes with NOP (e3) */
+    memset(codes, 0xe3, sizeof(codes));
+    p = codes;
+    ALLOC_S(208);
+    SAVE_FPLR(192);
+    ADD_FP(192);
+    SAVE_REG(19, 184);
+    SAVE_REG(20, 176);
+    SAVE_REG(21, 168);
+    SAVE_REG(22, 160);
+    SAVE_REG(23, 152);
+    SAVE_REG(24, 144);
+    SAVE_REG(25, 136);
+    SAVE_REG(26, 128);
+    SAVE_REG(27, 120);
+    SAVE_REG(28, 112);
+    SAVE_FREG(8, 104);
+    SAVE_FREG(9, 96);
+    SAVE_FREG(10, 88);
+    SAVE_FREG(11, 80);
+    SAVE_FREG(12, 72);
+    SAVE_FREG(13, 64);
+    SAVE_FREG(14, 56);
+    SAVE_FREG(15, 48);
+    /* end of unwinding codes */
+    *p++ = 0xe4;
+    /* codes must be aligned on 4-byte word */
+    const size_t unwindsize1 = ((p - codes) + 3) & ~3;
+
+    ui.X = 1;
+    ui.CodeWords = unwindsize1 / 4;
+    ui.FunctionLength = fcofs / 4;
+    owrite(ctx, &ui, sizeof(ui));
+    owrite(ctx, &codes, unwindsize1);
+    handler[0] = 0;
+    handler[1] = 0xffffffff;
+    owrite(ctx, &handler, sizeof(handler));
+
+    memset(&ui, 0, sizeof(ui));
+    memset(codes, 0xe3, sizeof(codes));
+
+    p = codes;
+    SAVE_FPLR(16);
+    ADD_FP(16);
+    SAVE_REG_X(19, -24);
+    SAVE_REG_X(20, -32);
+    *p++ = 0xe4;
+    const size_t unwindsize2 = ((p - codes) + 3) & ~3;
+
+    ui.CodeWords = unwindsize2 / 4;
+    ui.FunctionLength = (uint32_t)(ctx->codesz - fcofs) / 4;
+    owrite(ctx, &ui, sizeof(ui));
+    owrite(ctx, &codes, unwindsize2);
+
+    reloc.vaddr = sizeof(ui)+unwindsize1; reloc.symidx = 1+2+nrsym+2+2;
+    reloc.type = PEOBJ_RELOC_ADDR32NB;
+    owrite(ctx, &reloc, PEOBJ_RELOC_SIZE);
+  }
+#endif
 #elif LJ_TARGET_X86
   /* Write .sxdata section. */
   for (i = 0; i < nrsym; i++) {
@@ -346,7 +502,7 @@ void emit_peobj(BuildCtx *ctx)
       emit_peobj_sym(ctx, ctx->relocsym[i], 0,
 		     PEOBJ_SECT_UNDEF, PEOBJ_TYPE_FUNC, PEOBJ_SCL_EXTERN);
 
-#if LJ_TARGET_X64
+#if LJ_TARGET_X64 || LJ_TARGET_ARM64
     emit_peobj_sym_sect(ctx, pesect, PEOBJ_SECT_PDATA);
     emit_peobj_sym_sect(ctx, pesect, PEOBJ_SECT_XDATA);
     emit_peobj_sym(ctx, "lj_err_unwind_win", 0,
