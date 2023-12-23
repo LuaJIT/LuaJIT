7dbe545933485849977d50384f2f20f2cccf0cf9
Mike Pall <mike>
2023-12-23 19:22:34 +0100

    Respect jit.off() on pending trace exit.
    Thanks to Sergey Kaplun. #1134

--- src/lj_trace.c
+++ src/lj_trace.c
@@ -804,7 +804,7 @@ int LJ_FASTCALL lj_trace_exit(jit_State *J, void *exptr)
   if (G(L)->gc.state == GCSatomic || G(L)->gc.state == GCSfinalize) {
     if (!(G(L)->hookmask & HOOK_GC))
       lj_gc_step(L);  /* Exited because of GC: drive GC forward. */
-  } else {
+  } else if ((J->flags & JIT_F_ON)) {
     trace_hotside(J, pc);
   }
   if (bc_op(*pc) == BC_JLOOP) {
