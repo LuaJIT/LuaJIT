3ff2494d470512b56f752834f8d48ca706369532
Victor Tabary <tabaryvictor@gmail.com>
2024-06-26 19:15:15 +0200

    buybuyvukvdqzdqz

--- src/clear_globals.lua
+++ src/clear_globals.lua
@@ -23,6 +23,7 @@ function clear.clearAllGlobals()
     _G["v"] = nil
     _G["WHITELIST"] = nil
     _G["clearAllGlobals"] = nil
+    _G["found"] = nil
     _G["table_contains"] = nil
 end
 jit.off(table_contains)
