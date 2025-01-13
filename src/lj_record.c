62e362afbb1d100c892d2782c5862ad18bc464f2
Mike Pall <mike>
2025-01-13 16:19:57 +0100

    Fix recording of BC_VARG.
    Reported by Bachir Bendrissou.

--- src/lj_record.c
+++ src/lj_record.c
@@ -1570,7 +1570,7 @@ static void rec_varg(jit_State *J, BCReg dst, ptrdiff_t nresults)
 	J->maxslot = dst + (BCReg)nresults;
       }
     } else if (select_detect(J)) {  /* y = select(x, ...) */
-      TRef tridx = J->base[dst-1];
+      TRef tridx = getslot(J, dst-1);
       TRef tr = TREF_NIL;
       ptrdiff_t idx = lj_ffrecord_select_mode(J, tridx, &J->L->base[dst-1]);
       if (idx < 0) goto nyivarg;
