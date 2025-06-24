17601e4c66e95ae5ff26d2bd3e00936effbcd0ff
Regen1337 <125764136+Regen1337@users.noreply.github.com>
2025-06-24 06:39:39 -0400

    patch-3

--- src/lj_tab.c
+++ src/lj_tab.c
@@ -382,12 +382,10 @@ cTValue * LJ_FASTCALL lj_tab_getinth(GCtab *t, int32_t key)
   k.n = (lua_Number)key;
   n = hashnum(t, &k);
   do {
-    if (!n || (uintptr_t)&n->key < 0x1000 || (uintptr_t)&n->key > 0xFFFFFFFF) {
+    if (!n || (uintptr_t)&n->key < 0x1000 || (uintptr_t)&n->key > 0xFFFFFFFF)
       break;
-    }
-    if (tvisnum(&n->key) && n->key.n == k.n) {
+    if (tvisnum(&n->key) && n->key.n == k.n)
       return &n->val;
-    }
   } while ((n = nextnode(n)) != NULL);
   return NULL;
 }
