99394a484e7aef877492f8a38a661c29bf9f05d5
gns <wangbingzhen.riscv@isrc.iscas.ac.cn>
2025-11-26 16:25:31 +0800

    riscv(misc): add bytecode listing support

--- src/jit/bcsave.lua
+++ src/jit/bcsave.lua
@@ -101,6 +101,7 @@ local map_arch = {
   mips64el =	{ e = "le", b = 64, m = 8, f = 0x80000007, },
   mips64r6 =	{ e = "be", b = 64, m = 8, f = 0xa0000407, },
   mips64r6el =	{ e = "le", b = 64, m = 8, f = 0xa0000407, },
+  riscv64 =    { e = "le", b = 64, m = 243, f = 0x00000004, },
 }
 
 local map_os = {
